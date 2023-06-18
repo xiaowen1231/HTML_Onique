e4cebc63a9bc33e0187e7d6b16722ff3db2ee4ba
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-06-18 16:24:29 +0800

    20230618

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,10 +1,32 @@
 let index = 0
 
+let imageCount = document.querySelectorAll(
+    ".carousel .container img"
+).length
+
+const bottom = document.querySelector(".carousel .bottom")
+for(let i = 0;i<imageCount;i++){
+    const indicator = document.createElement("div")
+    indicator.classList.add("indicator")
+    indicator.onclick=()=>setIndex(i)
+
+    bottom.append(indicator)
+}
+
+function createAuto(){
+    return setInterval(() => {
+        index++
+        refresh()
+    }, 3000)
+}
+
+let autoTimer = createAuto()
+
 function refresh(){
     if(index<0){
-        index=2
+        index=imageCount-1
     }
-    else if(index>2){
+    else if(index>imageCount-1){
         index=0
     }
 
@@ -22,6 +44,9 @@ let refreshWrapper = (func) => {
     return function (...args){
         let result = func(...args)
         refresh()
+
+        clearInterval(autoTimer)
+        autoTimer = createAuto()
         return result
     }
 }
